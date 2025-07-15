af3f1ddcc2ed09018236008f667fe6679a3baf1a
sayan93-eng <52000338+sayan93-eng@users.noreply.github.com>
2025-07-15 19:09:33 +0530

    CartCount

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -241,6 +241,17 @@ function ProductList({ onHomeClick }) {
         textDecoration: 'none',
     }
 
+    const styleCartCount = {
+        position: 'absolute',
+        top: '40px',
+        right: '40px',
+        color: 'white',
+        borderRadius: '50%',
+        padding: '2px 5px',
+        fontSize: '15px',
+        fontWeight: 'bold',
+    };
+
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -289,7 +300,11 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg>
+                    </h1>
+                    {/* Cart count badge */}
+                    {cartCount > 0 && <span className="cart-count" style={styleCartCount}>{cartCount}</span>}
+                    </a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -316,7 +331,7 @@ function ProductList({ onHomeClick }) {
                                 onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
                                 disabled={!!addedToCart[plant.name]} // Disable button if already added to cart
                             >
-                                {addedToCart[plant.name] ? 'Added' : 'Add to Cart'}
+                                {addedToCart[plant.name] ? 'Added to Cart' : 'Add to Cart'}
                             </button>
                             </div>
                         ))}
